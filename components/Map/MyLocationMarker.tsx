14e0721ec2a647a694c3af4b79894f0bd4f9cecb
ArionAB <mihai.arion0@gmail.com>
2024-02-03 10:44:23 +0200

    my location button is can now be toggled to recenter you always

--- components/Map/MyLocationMarker.tsx
+++ components/Map/MyLocationMarker.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { Circle, Marker, Popup, useMap, useMapEvents, } from 'react-leaflet'
 import L, { divIcon } from 'leaflet';
 import { renderToStaticMarkup } from 'react-dom/server';
@@ -6,27 +6,28 @@ import MyLocationIcon from '@mui/icons-material/MyLocation';
 import { Fab } from '@mui/material';
 import { LocationModel } from '@/utils/Store/Models/Location/LocationModel';
 import { useDeviceOrientation } from '../../utils/Hooks/useDeviceOrientation';
+import { flyToLocation } from './FlyToLocation';
 
 
 export const MyLocationMarker: FC<{ position: LocationModel, accuracy: number }> = ({
     position,
     accuracy
 }) => {
+    const [isTracking, setIsTracking] = useState(false)
     const map = useMap()
-    const mapEvents = useMapEvents({
-        click: () => {
-            map.locate()
-        },
-        locationfound: (location: any) => {
-            console.log('location found:', location)
-        },
-    });
+
+
+    useEffect(() => {
+        if (isTracking) {
+            flyToLocation(map, position.lat, position.lng)
+        }
+    }, [position, isTracking])
 
     const { orientation, requestAccess, revokeAccess, error } = useDeviceOrientation();
     const iconHTML = renderToStaticMarkup(
         <div>
             <svg width="20px" height="20px" viewBox="0 0 24 24" fill="#4285F4">
-                <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
         </div>
     );
@@ -41,15 +42,15 @@ export const MyLocationMarker: FC<{ position: LocationModel, accuracy: number }>
             {/* @ts-ignore */}
             <Marker position={position} icon={customMarkerIcon}>
                 {/* @ts-ignore */}
-                <Circle center={position} radius={accuracy} color="#4285F4" fillColor="rgb(66, 133, 244, .4)"></Circle>
+                <Circle center={position} radius={accuracy} ></Circle>
                 <Popup>You are here</Popup>
             </Marker>
             <Fab
-                onClick={() => console.log('asd')}
+                onClick={() => setIsTracking(!isTracking)}
                 sx={{
                     position: 'absolute', right: 0, zIndex: '999', margin: '1rem', backgroundColor: "#eaeaea", padding: "1rem", borderRadius: "50%"
                 }} >
-                <MyLocationIcon />
+                <MyLocationIcon color={isTracking ? 'success' : 'inherit'} />
             </Fab>
         </>
 
