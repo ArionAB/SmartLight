496839cc8a8cc15d87101bea42ef7a7f1f8b7e43
ArionAB <mihai.arion0@gmail.com>
2025-02-10 17:19:15 +0200

    v3 for fixing android problem

--- components/FileUpload/FileUploadComponent.tsx
+++ components/FileUpload/FileUploadComponent.tsx
@@ -134,7 +134,7 @@ const FileUploadComponent: FC<FileUploadProps> = ({
                     elevation={isDragActive ? 15 : 5}
                     onClick={(e: any) => openExplorerDialog(e)}
                 >
-                    <input {...getInputProps()} capture="environment" />
+                    <input {...getInputProps()} type="file" accept="image/*,text/plain" />
                     {!hasFiles && (
                         <Grid
                             container
